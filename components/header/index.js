16edaea534a4468236ed74b66e86f382dc064cfa
VadimDmitr <vadzzimluch@gmail.com>
2024-01-17 20:37:59 -0500

    start project

---
+++ components/header/index.js
@@ -0,0 +1,28 @@
+import styles from './Header.module.css'
+import Link from 'next/link'; 
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>Logo</div>
+      <nav className={styles.nav}>
+       
+        <Link href="/" className={styles.navlink}>
+          Home
+        </Link>
+        <Link href="/about" className={styles.navlink}>
+          About
+        </Link>
+        <Link href="/services" className={styles.navlink}>
+          Services
+        </Link>
+        <Link href="./contact" className={styles.navlink}>
+          Contact
+        </Link>
+      </nav>
+      <button className={styles.menubutton}>Menu</button>
+    </header>
+  );
+};
+
+export default Header;
