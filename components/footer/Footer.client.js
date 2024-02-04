ab591184284fc20702d2bf3daed7faf3125c3816
VadimDmitr <vadzzimluch@gmail.com>
2024-02-04 12:25:47 -0500

    style footer

--- components/footer/Footer.client.js
+++ components/footer/Footer.client.js
@@ -1,36 +1,41 @@
-'use client';
-
 import React from 'react';
 import { FaYoutube } from 'react-icons/fa';
 import styles from './Footer.module.css';
 
 const Footer = () => {
+  // Переменные перемещены на уровень компонента для доступности
+  const emailTo = 'vadzzimluch@gmail.com';
+  const subject = encodeURIComponent('New Inquiry via Sidekick Website');
+  const body = encodeURIComponent(
+    "Hello, I'm interested in discussing collaboration opportunities related to website development",
+  );
+
   return (
     <footer className={styles.footer}>
       <div className={styles.subscribeSection}>
         <span>SUBSCRIBE TO OUR </span>
-        <div className={styles.youtubeLinkContainer}>
-          <a
-            href="https://www.youtube.com/@sidekickfilmco.273"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.link}
-          >
-            SIDEKICK YOUTUBE
-          </a>
-          <a
-            href="https://www.youtube.com/@sidekickfilmco.273"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.iconLink}
-          >
-            <FaYoutube size="1.4em" />
-          </a>
-        </div>
+        <a
+          href="https://www.youtube.com/@sidekickfilmco.273"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.combinedLink}
+        >
+          <span className={styles.textWithMargin}>SIDEKICK YOUTUBE</span>
+          <FaYoutube size="1.4em" />
+        </a>
         <span> CHANNEL</span>
       </div>
       <div className={styles.copyRight}>
-        <div>BUILT WITH THOUGHT BY VADKO</div>
+        <div className={styles.copyRightText}>
+          BUILT WITH THOUGHT BY
+          <a
+            href={`mailto:${emailTo}?subject=${subject}&body=${body}`}
+            className={styles.emailLink}
+          >
+            VADKO
+          </a>
+        </div>
+
         <div>COPYRIGHT © 2024 ALL RIGHTS RESERVED.</div>
       </div>
     </footer>
