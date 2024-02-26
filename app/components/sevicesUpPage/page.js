a5f9a9419ccb64e814bdc37b3f4141b80eea9cc6
VadimDmitr <vadzzimluch@gmail.com>
2024-02-26 15:46:34 -0500

    add servicesUpPage

---
+++ app/components/sevicesUpPage/page.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './servicesUp.module.css';
+
+const ServicesUpPage = () => {
+  return (
+    <div className={styles.contentContainer} id="services">
+      <h1>Services</h1>
+      <h2>Why Video Matters…</h2>
+      <p>
+        Everyone with a Smart Phone is seeing video content. Over 254 million
+        Americans watch YouTube. Did you know, a whopping 73.3% of target
+        consumers 25-55 years-old watch YouTube content weekly. Video is
+        powerful, delivers the biggest bang for the buck, and is here to stay.
+        It’s your time to leverage video—a multi-generational medium that’s
+        sharable with an email link, in a text, on your website, on social
+        media, or as a QR code.
+      </p>
+    </div>
+  );
+};
+
+export default ServicesUpPage;
