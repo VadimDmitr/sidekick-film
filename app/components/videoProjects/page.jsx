b3fbe191f3fa03a76fd247dde849cf7a8cca16a4
VadimDmitr <vadzzimluch@gmail.com>
2024-02-28 22:22:36 -0500

    add react-player

--- app/components/videoProjects/page.jsx
+++ app/components/videoProjects/page.jsx
@@ -1,57 +1,65 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
 import styles from './videoProjects.module.css';
 
 const VideoComponent = () => {
+  const [isClient, setIsClient] = useState(false);
+  const [playing, setPlaying] = useState(Array(8).fill(false)); // Step 1: Initialize playing states for 8 videos
+
+  useEffect(() => {
+    setIsClient(true); // This will set `isClient` to true in the client-side environment after the component mounts
+  }, []);
+
   const videos = [
     {
       title: 'YouTube Playlist',
-      url: 'https://www.youtube.com/embed/b553Rwefdlk',
+      url: 'https://www.youtube.com/watch?v=b553Rwefdlk',
     },
-    {
-      title: `Video 2`,
-      url: `https://www.youtube.com/embed/4GaT6yzMZfE`,
-    },
-    {
-      title: `Video 3`,
-      url: `https://www.youtube.com/embed/mwfcMlb_BOk`,
-    },
-    {
-      title: `Video 4`,
-      url: `https://www.youtube.com/embed/8RAzQirQ8KI`,
-    },
-    {
-      title: `Video 5`,
-      url: `https://www.youtube.com/embed/nhTHjLI5Ur0`,
-    },
-    {
-      title: `Video 6`,
-      url: `https://www.youtube.com/embed/OPj9s2VBEjw`,
-    },
-    {
-      title: `Video 7`,
-      url: `https://www.youtube.com/embed/i4tqR9jqqnY`,
-    },
-    {
-      title: `Video 8`,
-      url: `https://www.youtube.com/embed/ZhmizQG5Iz0`,
-    },
+    { title: 'Video 2', url: 'https://www.youtube.com/watch?v=4GaT6yzMZfE' },
+    { title: 'Video 3', url: 'https://www.youtube.com/watch?v=mwfcMlb_BOk' },
+    { title: 'Video 4', url: 'https://www.youtube.com/watch?v=8RAzQirQ8KI' },
+    { title: 'Video 5', url: 'https://www.youtube.com/watch?v=nhTHjLI5Ur0' },
+    { title: 'Video 6', url: 'https://www.youtube.com/watch?v=OPj9s2VBEjw' },
+    { title: 'Video 7', url: 'https://www.youtube.com/watch?v=i4tqR9jqqnY' },
+    { title: 'Video 8', url: 'https://www.youtube.com/watch?v=ZhmizQG5Iz0' },
   ];
+
+  if (!isClient) {
+    return <div>Loading videos...</div>;
+  }
+
+  const handleVideoClick = (index) => {
+    const newPlaying = [...playing];
+    newPlaying[index] = true;
+    setPlaying(newPlaying);
+  };
 
   return (
     <div className={styles.container} id="ourWork">
       <div className={styles.videoTitle}>
         <p>Sidekick Projects</p>
-        {/* <Image src="/arrow-image.png" alt="Arrow" width={50} height={50} /> */}
       </div>
       <div className={styles.videosGrid}>
         {videos.map((video, index) => (
-          <div key={index} className={styles.videoItem}>
-            <iframe
-              loading="lazy"
-              className={styles.iframeStyle}
-              src={video.url}
-              allow="fullscreen"
-              title={video.title}
-            ></iframe>
+          <div
+            key={index}
+            className={styles.videoItem}
+            onClick={() => handleVideoClick(index)}
+          >
+            <ReactPlayer
+              className={styles.reactPlayer}
+              url={video.url}
+              width="100%"
+              height="100%"
+              controls={true}
+              light={true}
+              playing={playing[index]}
+              loop={false}
+              playbackRate={1.0}
+              volume={0.8}
+              muted={false}
+            />
           </div>
         ))}
       </div>
