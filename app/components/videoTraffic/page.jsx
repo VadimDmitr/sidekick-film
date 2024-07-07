a8ff01f54fc2f58430f3e6b8563fa0aa1ad6d0f3
VadimDmitr <vadzzimluch@gmail.com>
2024-07-07 09:09:18 -0400

    Change down Videoloop

--- app/components/videoTraffic/page.jsx
+++ app/components/videoTraffic/page.jsx
@@ -16,7 +16,7 @@ export default function VideoTraffic() {
         className={styles.backgroundVideo}
         // onLoadedData={(e) => e.target.play()}
       >
-        <source src="/videoTraffic2.mp4" type="video/mp4" />
+        <source src="/videoMountains.mp4" type="video/mp4" />
         Your browser does not support the video tag
       </video>
       {/* <h1 className={styles.videoTrafficTitle}>Video Muscle</h1> */}
