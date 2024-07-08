80704593a1daa86da2aee2c29ca4efa5788cba59
VadimDmitr <vadzzimluch@gmail.com>
2024-07-08 10:06:39 -0400

    Remove loop from videoMuscle section

--- app/components/videoMuscle/page.jsx
+++ app/components/videoMuscle/page.jsx
@@ -1,16 +1,15 @@
 'use client';
 import React from 'react';
 import styles from './videoMuscle.module.css';
-// import { useInView } from 'react-intersection-observer';
 
 export default function VideoMuscle() {
   return (
     <div className={styles.servicesContainer}>
-      <video autoPlay muted loop playsInline className={styles.backgroundVideo}>
+      <video autoPlay muted playsInline className={styles.backgroundVideo}>
         <source src="/videoMuscle1.mp4" type="video/mp4" />
         Your browser does not support the video tag
       </video>
-      <h1 className={styles.serviceTitle}>Video Muscle</h1>
+      {/* <h1 className={styles.serviceTitle}>Video Muscle</h1> */}
     </div>
   );
 }
