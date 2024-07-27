3ed689c56f37af63fcff858dd3afab2b40d5b32d
VadimDmitr <vadzzimluch@gmail.com>
2024-07-27 16:02:03 -0400

    Animation videoFlag component

--- app/components/videoFlag/page.jsx
+++ app/components/videoFlag/page.jsx
@@ -1,12 +1,28 @@
 'use client';
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './videoFlag.module.css';
 
 export default function VideoFlag() {
+  const videoRef = useRef(null);
+
+  const handleVideoClick = () => {
+    if (videoRef.current) {
+      videoRef.current.currentTime = 3;
+      videoRef.current.play();
+    }
+  };
+
   return (
     <div className={styles.servicesContainer}>
-      <video autoPlay muted playsInline className={styles.backgroundVideo}>
-        <source src="/videoMuscle1.mp4" type="video/mp4" />
+      <video
+        ref={videoRef}
+        autoPlay
+        muted
+        playsInline
+        className={styles.backgroundVideo}
+        onClick={handleVideoClick}
+      >
+        <source src="/videoFlag.mp4" type="video/mp4" />
         Your browser does not support the video tag
       </video>
     </div>
