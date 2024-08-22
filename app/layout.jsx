1e009d89735f43642de155c88f6ce7a5b76a41ca
VadimDmitr <vadzzimluch@gmail.com>
2024-08-21 20:43:13 -0400

    Add metatags

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,6 +10,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>Professional Video Production Services | Sidekick Media USA</title>
+        <meta
+          name="description"
+          content="Sidekick Media offers professional video production services in Raleigh, Cary, Durham, Research Triangle Area (RTP), North Carolina. Get noticed with our high-quality creative video content."
+        />
+        <meta name="theme-color" content="#000000" />
         {/* <link rel="apple-touch-icon" href="/favicon.png"></link>
         <link
           rel="icon"
