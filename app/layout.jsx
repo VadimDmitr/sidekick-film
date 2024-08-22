39ecb5a49719c44e9878cbe06019a613b9fed916
VadimDmitr <vadzzimluch@gmail.com>
2024-08-21 21:25:06 -0400

    Add link favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,16 +15,14 @@ export default function RootLayout({ children }) {
         </title>
         <meta
           name="description"
-          content="Sidekick Media offers professional video production services in Raleigh, Cary, Durham, Research Triangle Area (RTP), North Carolina. Get noticed with our high-quality creative video content."
+          content="Sidekick Media offers professional video production services in Cary, North Carolina. Get noticed with our high-quality creative video content for businesses of all sizes."
         />
         <meta name="theme-color" content="#000000" />
-        {/* <link rel="apple-touch-icon" href="/favicon.png"></link>
-        <link
-          rel="icon"
-          type="image/x-icon"
-          href="/favicon.png"
-          sizes="any"
-        /> */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" sizes="any" />
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" /> */}
       </head>
       <body
         className={robotoCondensed.className}
