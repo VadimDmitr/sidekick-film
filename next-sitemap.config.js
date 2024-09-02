17eab79d20d3e21a10ac247f2a7a3115bbcc774b
VadimDmitr <vadzzimluch@gmail.com>
2024-09-02 10:49:05 -0400

    Add sitemap files for browsers search

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.sidekickmediausa.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './public',
+};
